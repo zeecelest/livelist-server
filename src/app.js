8fe3a51b1c892d632ac8033e8ae2a04c1f5beeae
julio hernandez <hernandezjulio58@ymail.com>
2020-01-06 11:46:16 -0800

    added folders w/middleware

--- src/app.js
+++ src/app.js
@@ -1,31 +1,25 @@
-require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const errorHandler = require('./middleware/error-handler');
+const authRouter = require('./auth/auth-router');
+const userRouter = require('./user/user-router');
 
 const app = express();
 
-const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
-
-app.use(morgan(morganOption));
-app.use(helmet());
+app.use(
+  morgan(NODE_ENV === 'production' ? 'tiny' : 'common', {
+    skip: () => NODE_ENV === 'test'
+  })
+);
 app.use(cors());
+app.use(helmet());
 
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
-});
+app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
 
-app.get('/', (req, res) => {
-  res.send('Hello, boilerplate!');
-});
+app.use(errorHandler);
 
 module.exports = app;
