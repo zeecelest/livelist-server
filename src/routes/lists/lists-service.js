0fa2a52ef569cd19020175dbbd390ddb44129ff3
julio hernandez <hernandezjulio58@ymail.com>
2020-01-07 11:24:27 -0800

    lists-service sketched out

--- src/routes/lists/lists-service.js
+++ src/routes/lists/lists-service.js
@@ -1,5 +1,31 @@
 const ListService = {
-  ////
+  getAllLists(knex) {
+    return knex.select('*').from('lists');
+  },
+  insertListmark(knex, newList) {
+    return knex
+      .insert(newList)
+      .into('lists')
+      .returning('*')
+      .then(rows => rows[0]);
+  },
+  getById(knex, id) {
+    return knex
+      .from('lists')
+      .select('*')
+      .where('id', id)
+      .first();
+  },
+  deleteListmark(knex, id) {
+    return knex('lists')
+      .where({ id })
+      .delete();
+  },
+  updatelists(knex, id, newListField) {
+    return knex('lists')
+      .where({ id })
+      .update(newListField);
+  }
 };
 
 module.exports = ListService;
