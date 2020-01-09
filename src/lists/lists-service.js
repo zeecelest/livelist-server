54cf2621b308e95eea021e03d0f00f1a32354902
Daniel Bright <brahyt@gmail.com>
2020-01-09 09:22:04 -0800

    fixed lists/:id

--- src/lists/lists-service.js
+++ src/lists/lists-service.js
@@ -51,10 +51,30 @@ const ListService = {
   },
   getListById(knex, id) {
     return knex
-      .from('lists')
-      .select('*')
-      .where({ id })
-      .first();
+      .raw(`
+        SELECT lists_spots.list_id,
+               lists.name AS list_name,
+               lists.tags AS list_tags,
+               users.name AS created_by,
+               spot_id,
+               spots.name,
+               spots.tags AS spots_tags,
+               spots.address,
+               spots.city,
+               spots.state,
+               spots.lat,
+               spots.lon AS lng
+               FROM lists_spots
+               JOIN spots
+               ON spot_id = spots.id
+               JOIN lists
+               ON list_id = lists.id
+               JOIN users_lists
+               ON lists_spots.list_id = users_lists.list_id
+               JOIN users
+               ON users_lists.users_id = users.id
+               WHERE lists_spots.list_id = ${id};
+    `)
   },
   deleteListReference(knex, list_id, user_id) {
     return knex('users_lists')
