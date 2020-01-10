261f65c17241ad86828a6c19930111f5fcbca229
julio hernandez <hernandezjulio58@ymail.com>
2020-01-10 11:09:45 -0800

    breaking the tests now :)

--- test/lists-endpoint.spec.js
+++ test/lists-endpoint.spec.js
@@ -23,21 +23,49 @@ describe('Lists Endpoint', function() {
           .get('/api/lists')
           .expect(401, { error: `Missing bearer token` });
       });
+      //   it(`responds 401 'Unauthorized request' when invalid JWT secret`, () => {
+      //     const validUser = helpers.makeUsersArray()[0];
+      //     console.log(validUser);
+      //     const invalidSecret = 'bad-secret';
+      //     return supertest(app)
+      //       .get('/api/lists')
+      //       .set(
+      //         'Authorization',
+      //         helpers.makeAuthHeader(validUser, invalidSecret)
+      //       )
+      //       .expect(401, { error: `Unauthorized request` });
+      //   });
+      //   it(`responds 401 'Unauthorized request' when invalid sub in payload`, () => {
+      //     const invalidUser = { user_name: 'user-not-existy', id: 1 };
+      //     return supertest(app)
+      //       .get('/api/lists')
+      //       .set('Authorization', helpers.makeAuthHeader(invalidUser))
+      //       .expect(401, { error: `Unauthorized request` });
+      //   });
+      // });
+
+      // context(`Given Auth header`, () => {
+      //   it(`respongs with 200 and the lists`, () => {
+      //     return supertest(app)
+      //       .get('/api/lists')
+      //       .expect(200, { error: `Missing bearer token` });
+      //   });
+      // });
+
+      // context('Given there are lists in the database', () => {
+      //   beforeEach('insert lists', () =>
+      //     helpers.seedlistsTables(db, testUsers, testlists, testComments)
+      //   );
+
+      //   it('responds with 200 and all of the lists', () => {
+      //     const expectedlists = testlists.map((article) =>
+      //       helpers.makeExpectedArticle(testUsers, article, testComments)
+      //     );
+      //     return supertest(app)
+      //       .get('/api/lists')
+      //       .expect(200, expectedlists);
+      //   });
+      // });
     });
-
-    // context('Given there are lists in the database', () => {
-    //   beforeEach('insert lists', () =>
-    //     helpers.seedlistsTables(db, testUsers, testlists, testComments)
-    //   );
-
-    //   it('responds with 200 and all of the lists', () => {
-    //     const expectedlists = testlists.map((article) =>
-    //       helpers.makeExpectedArticle(testUsers, article, testComments)
-    //     );
-    //     return supertest(app)
-    //       .get('/api/lists')
-    //       .expect(200, expectedlists);
-    //   });
-    // });
   });
 });
