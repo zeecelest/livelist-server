9335ad6768262fab36dd67b3bdd0c9fcf83b9972
Daniel Bright <brahyt@gmail.com>
2020-01-09 16:02:47 -0800

    GEOCODEING THE WORLD!

--- src/spots/spots-router.js
+++ src/spots/spots-router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const SpotsService = require('./spots-service');
 const { requireAuth } = require('../middleware/jwt-auth');
-
+const https = require('https')
 const spotsRouter = express.Router();
 const jsonBodyParser = express.json();
+const API_KEY = process.env.API_GEO_KEY
+
 
 spotsRouter
   .use(requireAuth)
@@ -17,29 +19,56 @@ spotsRouter
       next(error);
     }
   })
-  .post(jsonBodyParser, (req, res, next) => {
-    const { list_id, name, address, city, state } = req.body;
-    for (const field of ['list_id', 'name', 'city', 'state', 'address'])
-      if (!req.body[field])
-        return res.status(400).json({
-          error: `Missing '${field}' in request body`
-        });
-    try {
-      let newSpot = {
-        address,
-        city,
-        lat,
-        tags,
-        lon,
-        name,
-        state
-      };
-      SpotsService.insertSpot(req.app.get('db'), newSpot).then(spot => {
-        res.status(200).json(spot);
-      });
-    } catch (error) {
-      next(error);
-    }
+  .post (jsonBodyParser, (req, res, next) => {
+    let { list_id, name, address, city, state, tags } = req.body;
+    address = address.replace(/ /g, '+');
+    city = city.replace(/ /g, '+');
+    https.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address},+${city},+${state}&key=${API_KEY}`, ress => {
+      ress.setEncoding("utf8");
+      let body = ''
+      ress.on("data", data => {
+        body += data
+      })
+      ress.on("end", () => {
+        body = JSON.parse(body)
+        //        console.log(body.results[0].geometry.location)
+        let newSpot = {
+          address,
+          city,
+          tags,
+          lat: body.results[0].geometry.location.lat,
+          lon: body.results[0].geometry.location.lng,
+          name,
+          state
+        };
+        return SpotsService.insertSpot(req.app.get('db'), newSpot, list_id)
+          .then(spot => {
+            console.log(spot)
+            return res.status(200).json(spot)
+          })
+      })
+    })
+    //    for (const field of ['list_id', 'name', 'city', 'state', 'address'])
+    //      if (!req.body[field])
+    //        return res.status(400).json({
+    //          error: `Missing '${field}' in request body`
+    //        });
+    //    try {
+    //      let newSpot = {
+    //        address,
+    //        city,
+    //        lat,
+    //        tags,
+    //        lon,
+    //        name,
+    //        state
+    //      };
+    //      SpotsService.insertSpot(req.app.get('db'), newSpot).then(spot => {
+    //        res.status(200).json(spot);
+    //      });
+    //    } catch (error) {
+    //      next(error);
+    //    }
   });
 spotsRouter
   .use(requireAuth)
--- src/spots/spots-service.js
+++ src/spots/spots-service.js
@@ -2,12 +2,58 @@ const SpotsService = {
   getAllSpots(knex) {
     return knex.select('*').from('spots');
   },
-  insertSpot(knex, newSpots, list_id) {
-    return knex
-      .insert(newSpot)
-      .into('spots')
-      .returning('*')
-      .then(rows => rows[0]);
+  deleteListReference(knex, list_id, users_id) {
+    return knex.transaction(trx => {
+      return knex('users_lists')
+        .transacting(trx)
+        .where({list_id})
+        .where({users_id})
+        .delete()
+        .then(res => {
+          return knex('lists_spots')
+            .transacting(trx)
+            .where({list_id})
+            .delete()
+            .then(res => {
+              return knex('lists')
+                .transacting(trx)
+                .where('id', list_id)
+                .delete()
+                .then(res => res)
+            })
+        })
+    })
+    //    return knex('users_lists')
+    //      .where({ list_id, users_id })
+    //      .delete();
+  },
+  insertSpot(knex, newSpot, list_id) {
+    return knex.transaction(trx => {
+      return knex('spots')
+        .transacting(trx)
+        .insert(newSpot)
+        .returning('*')
+        .then(res => {
+          console.log(res)
+          return knex('lists_spots')
+            .transacting(trx)
+            .insert({
+              list_id: list_id,
+              spot_id: res[0].id
+            })
+            .returning('*')
+            .then(res2 => {
+              return {
+                list_id,
+                ...res[0]
+              }
+            })
+        })
+    })
+    //      .insert(newSpot)
+    //      .into('spots')
+    //      .returning('*')
+    //      .then(rows => rows[0]);
   },
   getSpotById(knex, id) {
     return knex
